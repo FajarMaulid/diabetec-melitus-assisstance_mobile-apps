a0bafa44c9a49406f17ae6b9633bd27f7ce9278a
FajarMaulid <mohammadfajarmaulid2004@mail.ugm.ac.id>
2024-10-20 10:43:28 +0700

    delete all directory

--- backend/myapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path('api/items/', views.ItemListCreate.as_view(), name='item-list-create'),
-]
